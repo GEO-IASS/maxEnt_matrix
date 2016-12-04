Fix bugs in the calculation of f and J.

J.py
@@ -5,6 +5,8 @@ def J(alpha, A, omega_n, omega, C_real_inv, C_imag_inv):
     import numpy as np
     import numpy.linalg
 
+    domega = omega[1] - omega[0]
+
     matrix = kernel.K_matrix_real(omega_n, omega)
     result = -np.transpose(matrix).dot(C_real_inv).dot(matrix)
 
@@ -13,5 +15,5 @@ def J(alpha, A, omega_n, omega, C_real_inv, C_imag_inv):
 
     Nomega = len(omega)
     for i in range(Nomega):
-        result[i, i] = result[i, i] - alpha/A[i]
+        result[i, i] = result[i, i] - alpha*domega/A[i]
     return result

f.py
@@ -5,10 +5,11 @@ def f(alpha, G_real, G_imag, A, omega_n, omega, C_real_inv, C_imag_inv):
     import numpy as np
     import numpy.linalg 
 
+    domega = omega[1] - omega[0]
     Nomega = len(omega)
     result = np.zeros(Nomega)
     for nw in range(Nomega):
-        result[nw] = -alpha*(1 + np.log(A[nw]/default.D(omega[nw])))
+        result[nw] = -alpha*domega*(1 + np.log(A[nw]/default.D(omega[nw])))
     
     matrix = kernel.K_matrix_real(omega_n, omega)
     vector_right = G_real - matrix.dot(A)